75a6b452d511f3e40fdb70890b5b8135f5f7dd0b
Pentala, Venu Madhav <vpentala@iu.edu>
2025-04-16 00:28:17 -0400

    fixed clean button

--- ui.py
+++ ui.py
@@ -265,7 +265,7 @@ def render_upload_page() -> None:
         with col1:
             if st.button("Start Cleaning", key="start_cleaning_button"):
                 logger.debug("Start Cleaning button clicked")
-                st.session_state.page = "Clean"
+                st.session_state.sidebar_page = "Clean"
                 save_auth_state()
                 st.rerun()
         with col2:
